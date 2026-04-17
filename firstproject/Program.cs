7efe37b2a48a0bb9e80bcadfd1751bca8547f1bd
Admin <Admin@DESKTOP-SRMBPCR>
2026-04-17 10:26:38 +0530

    Add project files.

---
+++ firstproject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WellCome to Hello World");
+        }
+    }
+}
